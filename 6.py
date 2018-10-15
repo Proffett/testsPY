1fc1586ca2f332ee5e72228ba6f17e529251d47b
proffett <evgeny.profet@gmail.com>
2018-10-15 22:41:07 +0400

    15.10

--- 6.py
+++ 6.py
@@ -1,6 +1,18 @@
-A = int(input())
-B = int(input())
-N = int(input())
-S = ((A * 100 + B) * N) % 100
-F = ((A * 100 + B) * N) // 100
-print(F, S)
+pervstolb = int(input())
+pervstr = int(input())
+vtorstolb = int(input())
+vtorstr = int(input())
+if vtorstolb == pervstolb - 1 and vtorstr == pervstr - 1:
+    print('YES')
+elif vtorstolb == pervstolb - 1 and vtorstr == pervstr + 1:
+    print('YES')
+elif vtorstolb == pervstolb + 1 and vtorstr == pervstr + 1:
+    print('YES')
+elif vtorstolb == pervstolb + 1 and vtorstr == pervstr - 1:
+    print('YES')
+elif vtorstolb == pervstolb and (vtorstr == pervstr - 1 or vtorstr == pervstr + 1):
+    print('YES')
+elif (vtorstolb == pervstolb + 1 or vtorstolb == pervstolb - 1) and vtorstr == pervstr:
+    print('YES')
+else:
+    print('NO')
--- 7.py
+++ 7.py
@@ -1,3 +1,18 @@
-N = int(input())
-print('The next number for the number ', N, ' is ', N + 1, '.', sep='')
-print('The previous number for the number ', N, ' is ', N - 1, '.', sep='')
+pervstolb = int(input())
+pervstr = int(input())
+vtorstolb = int(input())
+vtorstr = int(input())
+if vtorstolb == pervstolb - 1 and vtorstr == pervstr - 1:
+    print('YES')
+elif vtorstolb == pervstolb - 1 and vtorstr == pervstr + 1:
+    print('YES')
+elif vtorstolb == pervstolb + 1 and vtorstr == pervstr + 1:
+    print('YES')
+elif vtorstolb == pervstolb + 1 and vtorstr == pervstr - 1:
+    print('YES')
+elif vtorstolb == pervstolb and (vtorstr == pervstr - 1 or vtorstr == pervstr + 1):
+    print('YES')
+elif (vtorstolb == pervstolb + 1 or vtorstolb == pervstolb - 1) and vtorstr == pervstr:
+    print('YES')
+else:
+    print('NO')
