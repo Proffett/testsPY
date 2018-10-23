8f761167258287951ef2969dfe291f98a6491265
evgeny lozhkin <montana.web@yandex.ru>
2018-10-23 11:17:34 +0400

    ok

--- LSIscript.py
+++ LSIscript.py
@@ -9,7 +9,7 @@ import pandas as pd
 from pymorphy2 import MorphAnalyzer
 
 m = MorphAnalyzer()
-query_text = "осевой вентилятор"
+query_text = "контекстная реклама"
 auth_token = "AQAAAAABN0XoAAU2LB8jCEqxKE2_umpAvbYZkTQ"
 url = 'https://api-sandbox.direct.yandex.ru/live/v4/json/'
 data = {'token': auth_token, 'method': 'CreateNewWordstatReport', 'param': {'Phrases': [query_text]}}
@@ -37,7 +37,7 @@ url_enc = urllib.parse.quote_plus(query_text)
 url_get = 'https://yandex.ru/search/xml?user=Syrexx&key=03.20399592:8ad8f41bc17d89c4525f5cbb0296f1e5&query=' + url_enc + '&l10n=ru&sortby=rlv&filter=strict&groupby=attr%3Dd.mode%3Ddeep.groups-on-page%3D50.docs-in-group%3D1'
 
 # путь к файлу, в которые запишутся все URL ТОП 50 выдачи по нашему запросу.
-sys.stdout = open("C:\\Users\\Evgen\\Documents\\url_list.txt", "w", encoding="utf-8")
+sys.stdout = open("C:\\Users\\user\\Documents\\url_list.txt", "w", encoding="utf-8")
 
 # Отправляем запрос и парсим XML выдачу:
 response = requests.get(url_get)
@@ -47,22 +47,13 @@ for url in soup.findAll('url'):
 
 # путь к файлу, куда запишем весь текст, полученный при парсинге страниц:
 sys.stdout.close()
-sys.stdout = open("C:\\Users\\Evgen\\Documents\\out.txt", "w", encoding="utf-8")
+sys.stdout = open("C:\\Users\\user\\Documents\\out.txt", "w", encoding="utf-8")
 
 # Нам лишь нужно найти наиболее часто употребляемые N-граммы в текстах ТОП 50.
-urls = [z.rstrip() for z in open('C:\\Users\\Evgen\\Documents\\Texts_Analyze\\url_list.txt', encoding='utf-8')]
-for url in urls:
-    try:
-        response = requests.get(url, timeout=None)
-    except:
-        continue
-    soup = BeautifulSoup(response.content, "html.parser")
 
-    for pp in soup.select("p"):
-        print(pp.text)
 
 # Приступаем к анализу полученных данных.
-urls = [z.rstrip() for z in open('C:\\Users\\Evgen\\Documents\\url_list.txt', encoding='utf-8')]
+urls = [z.rstrip() for z in open('C:\\Users\\user\\Documents\\url_list.txt')]
 for url in urls:
     try:
         response = requests.get(url, timeout=None)
@@ -75,9 +66,9 @@ for url in urls:
 
 # Указываем файл, куда запишем результаты, загружаем данные парсинга и список стоп-слов, которые мы хотим исключить из анализа N-грамм (предлоги, союзы, технические и коммерческие слова и т.д.)
 sys.stdout.close()
-sys.stdout = open("C:\\Users\\Evgen\\Documents\\result.txt", "w", encoding="utf-8")
-texts = [z.rstrip() for z in open('C:\\Users\\Evgen\\Documents\\out.txt', encoding='utf-8')]
-stop_words = [z.rstrip() for z in open('C:\\Users\\Evgen\\Documents\\stop_words.txt', encoding='utf-8')]
+sys.stdout = open("C:\\Users\\user\\Documents\\result.txt", "w", encoding="utf-8")
+texts = [z.rstrip() for z in open('C:\\Users\\user\\Documents\\out.txt', encoding='utf-8')]
+stop_words = [z.rstrip() for z in open('C:\\Users\\user\\Documents\\stop_words.txt', encoding='utf-8')]
 
 # приводим все слова к их исходной форме, настраиваем CountVectorizer на подсчет N-грамм с количеством слов от 2 до 4 и записываем результат в файл.
 cvn = CountVectorizer(ngram_range=(2, 4), stop_words=stop_words)
@@ -90,8 +81,8 @@ for ng_count, ng_text in sorted([(count_values[i], k) for k, i in vb.items()], r
     print(ng_text, ng_count, sep='\t')
 
 # Все, что нужно дальше, – записать все результаты в один Excel-файл:
-info_data = pd.read_csv('C:\\Users\\Evgen\\Documents\\result.txt', encoding='utf-8', sep="\t", header=None)
-filename = 'C:\\Users\\Evgen\\Documents\\' + query_text + '.xlsx'
+info_data = pd.read_csv('C:\\Users\\user\\Documents\\result.txt', encoding='utf-8', sep="\t", header=None)
+filename = 'C:\\Users\\user\\Documents\\' + query_text + '.xlsx'
 writer = pd.ExcelWriter(filename)
 info_data.to_excel(writer, 'NGrams', encoding='utf-8', index=False)
 sa_df.to_excel(writer, 'SearchedAlso', encoding='utf-8', index=False)
